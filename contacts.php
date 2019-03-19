4be16326b46068e03d7988884c42a6d03a509657
xorto1 <46969748+xorto1@users.noreply.github.com>
2019-03-19 15:52:20 +0000

    Update contacts.php

--- contacts.php
+++ contacts.php
@@ -3,14 +3,14 @@
 		//public function __construct(){
 		//	include ('config/connection.php');
 		//}
-		public function create_contact($Name, $Nick_Name, $Birthday, $Gender, $Address, $Phone_Number, $Email, $State_Of_Origin, $Tribe, $Likes, $Dislikes, $Favourite_Food, $Favourite_Colour, $Favourite_Movie, $Favourite_Musician, $Favourite_Course, $Favourite_Instructor, $Crush, $Picture){
+		public function create_contact($Name, $Nick_Name, $Birthday, $Gender, $Address, $Phone_Number, $Email, $Picture){
 			$host='localhost';
 			$user='root';
 			$password='';
 			$db='slumbook';
 			$connect = mysqli_connect($host, $user, $password, $db);
 			
-			$querry="insert into contacts values('$Name', '$Nick_Name', '$Birthday', '$Gender', '$Address', '$Phone_Number', '$Email', '$State_Of_Origin', '$Tribe', '$Likes', '$Dislikes', '$Favourite_Food', '$Favourite_Colour', '$Favourite_Movie', '$Favourite_Musician', '$Favourite_Course', '$Favourite_Instructor', '$Crush', 'James')";
+			$querry="insert into contacts values('$Name', '$Nick_Name', '$Birthday', '$Gender', '$Address', '$Phone_Number', '$Email')";
 			mysqli_query($connect, $querry) or die('could not insert' . mysqli_error($connect));
 			}
 		
